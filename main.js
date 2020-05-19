13e4fd08cd1c92bb9b2c586cb9d495a1a386bc43
nathaliavenerayan <47015176+nathaliavenerayan@users.noreply.github.com>
2020-05-18 21:31:05 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var full_name = "John Doe";
+var email = "JohnDoe@gmail.com";
+var tel = "6479108765";
+var address = "123 Street Street";
+var unit = "111";
+var zip = "M9J3J9";
+
+document.getElementById('order_billing_name').value = full_name;
+document.getElementById('order_email').value = email;
+document.getElementById('order_tel').value = tel;
+document.getElementById('bo').value = address;
+document.getElementById('oba3').value = unit;
+document.getElementById('order_billing_zip').value = zip;
