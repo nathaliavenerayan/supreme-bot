a9b36ef6824571ba64f6f0da2f57fff0332329b1
nathaliavenerayan <47015176+nathaliavenerayan@users.noreply.github.com>
2020-05-19 00:25:24 -0400

    added more getElementById elements

--- main.js
+++ main.js
@@ -6,15 +6,34 @@ var unit = "321";
 var zip = "M9J3J9";
 var city = "TORONTO";
 var country = "CANADA";
-var state = "ON";
+var CANADA_ZIP = states;
+var states = "ON";
 
-document.getElementById('order_billing_name').value = full_name;
-document.getElementById('order_email').value = email;
-document.getElementById('order_tel').value = tel;
-document.getElementById('bo').value = address;
-document.getElementById('oba3').value = unit;
-document.getElementById('order_billing_zip').value = zip;
-document.getElementById('order_billing_city').value = city;
-document.getElementById('order_billing_country').value = country;
-document.getElementById('states-CANADA').value = country;
-document.getElementById('order_billing_state').value = state;
+var number = "1234123412341234";
+var month = "04";
+var year = "2021";
+var three = "123";
+
+
+document.getElementById("order_billing_name").value = full_name;
+document.getElementById("order_email").value = email;
+document.getElementById("order_tel").value = tel;
+document.getElementById("bo").value = address;
+document.getElementById("oba3").value = unit;
+document.getElementById("order_billing_zip").value = zip;
+document.getElementById("order_billing_city").value = city;
+
+document.getElementById("states-CANADA").value = CANADA_ZIP;
+document.getElementById("order_billing_country").value = country;
+document.getElementById("order_billing_state").value = states;
+
+//document.getElementById("states-CANADA"
+//div class="input select optional order_billing_country"
+
+document.getElementById("rnsnckrn").value = number;
+document.getElementById("credit_card_month").value = month;
+document.getElementById("credit_card_year").value = year;
+document.getElementById("orcer").value = three;
+
+document.getElementsByClassName('iCheck-helper')[1].click();
+//document.getElementsByName("commit")[0].click();
